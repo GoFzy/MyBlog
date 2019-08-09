8bb74ad970e080e247f09653dbc4ea9fca8b1f2a
GoFzy <zyfeng@whu.edu.cn>
2019-08-09 11:31:02 +0800

    icon图标添加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,14 @@ const navbarConf = require('@navbar')
 const sidebarConf = require('@sidebar')
 
 module.exports = {
-    title: 'GoFzy',
-    description: '脚踏实地，仰望星空',
-    themeConfig: {
-        lastUpdated: '最后更新于：', // string | boolean
-        nav: navbarConf,
-        sidebar: sidebarConf
-    },
+	title: 'GoFzy',
+	description: '脚踏实地，仰望星空',
+	head: [
+		['link', { rel: "icon", href: "/F.png"}]
+	],
+	themeConfig: {
+		lastUpdated: '最后更新于：', // string | boolean
+		nav: navbarConf,
+		sidebar: sidebarConf
+	},
 }
